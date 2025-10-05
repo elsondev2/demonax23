cd7596ca1708e31349dd42939fffd3693dac12f9
justelson <elsonmgaya25@gmail.com>
2025-10-05 12:26:42 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'assets',
+  publicDir: 'public',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
