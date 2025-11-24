b6fbdf0f7d70455b6820ffeed33185f06220b0c3
justelson <elsonmgaya25@gmail.com>
2025-11-24 19:04:10 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -150,12 +150,12 @@ function AppContent() {
           <Route path="/eula" element={<Eulapage />} />
           <Route path="/games/checkers" element={
             <ProtectedRoute>
-
+              <CheckersGamePage />
             </ProtectedRoute>
           } />
           <Route path="/games/checkers/:gameId" element={
             <ProtectedRoute>
-
+              <CheckersGamePage />
             </ProtectedRoute>
           } />
           <Route path="/payment-instructions" element={
